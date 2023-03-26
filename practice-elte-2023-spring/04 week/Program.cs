3a30e032560bde2656d0f1fea916c2f451a33938
Euryn <crossdeltavi@gmail.com>
2023-03-26 19:25:08 +0200

    added exercises for week 4

---
+++ practice-elte-2023-spring/04 week/Program.cs
@@ -0,0 +1,171 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void monthNameToNumber()
+    {
+        // task 01:
+        // Write a program that gives the number of the month 
+        // if the name of the month is given.
+
+        string[] monthNames = {
+            "January ",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        };
+
+        string target = Console.ReadLine().ToLower();
+
+        for (int i = 0; i < monthNames.Length; i++)
+        {
+            if (monthNames[i] == target)
+            {
+                Console.Write($"The number of the month is = {i + 1}\n");
+                return;
+            }
+        }
+    }
+
+    static void computeOrdinalDay()
+    {
+        // Task 02:
+        // We have the number of a month and a day in that month. 
+        // Give me the ordinal number of that day in the year.
+
+        int[] offset = {
+            0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30
+        };
+
+        string? buffer;
+        string[] splitted_buffer;
+        int month, day, ordinal = 0;
+
+        Console.Write("Enter month and day separated by a space [Int Int]: ");
+        buffer = Console.ReadLine();
+        splitted_buffer = buffer.Split(" ");
+
+        month = Convert.ToInt32(splitted_buffer[0]);
+        day = Convert.ToInt32(splitted_buffer[1]);
+
+        for (int i = 0; i < month; i++)
+        {
+            ordinal = ordinal + offset[i];
+        }
+
+        ordinal = ordinal + day;
+
+        Console.Write($"The ordinal day is = {ordinal}\n");
+    }
+
+    static void processString()
+    {
+        // Task 03:
+        // A string is given.
+        //  a) Print the string backward.
+        //  b) Print every second character of the string.
+        //  c) Print it without syllables (a, e, i, o , u).
+
+        string buffer;
+        string reversed = "", second = "", vowels = "";
+        Console.Write("Enter a string:");
+
+        buffer = Console.ReadLine();
+
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            reversed = $"{buffer[i]}{reversed}";
+
+            if (i % 2 == 1)
+            {
+                second = $"{second}{buffer[i]}";
+            }
+
+            string lowered = buffer[i].ToString().ToLower();
+
+            if (lowered == "a" ||
+            lowered == "e" ||
+            lowered == "i" ||
+            lowered == "o" ||
+            lowered == "u")
+            {
+                vowels = $"{vowels}{buffer[i]}";
+            }
+        }
+        Console.Write($"Reversed = {reversed}\n");
+        Console.Write($"Second character = {second}\n");
+        Console.Write($"Vowels = {vowels}\n");
+    }
+
+    static void monogram()
+    {
+        // Task 04:
+        // We have a name. 
+        // Give the monogram of the name
+        // (the family and last name both start with capitals).
+
+        string buffer;
+        string[] splitted_buffer;
+        string first_name, last_name;
+        Console.Write("Enter first and last name separated by a space [String String]:");
+        buffer = Console.ReadLine();
+        splitted_buffer = buffer.Split(" ");
+
+        first_name = splitted_buffer[0];
+        last_name = splitted_buffer[1];
+
+        Console.Write($"Monogram = {first_name[0]}{last_name[0]}\n");
+    }
+
+    static void countEven()
+    {
+        // Task 05:
+        // NOTE: Skipped
+        // There is an array of N length (1<=N<=100). 
+        // How many even numbers are there in the array?
+
+        // string buffer;
+        // int n, count = 0;
+        // Console.Write("Enter a number N [Int]:");
+        // buffer = Console.ReadLine();
+
+        // n = Convert.ToInt32(buffer);
+
+        // for (int i = 0; i < )
+    }
+
+    static void sumArray()
+    {
+        // Task: 06
+        // NOTE: Skipped
+        // There is an array of N length (1<=N<=100). 
+        // What is the sum of all the elements?
+    }
+
+    static void hasEven()
+    {
+        // Task: 07
+        // NOTE: Skipped
+        // There is an array of N length (1<=N<=100). 
+        // Is there an even number in the array?
+    }
+
+
+    static void Main()
+    {
+        // computeOrdinalDay();
+        // processString();
+
+        monogram();
+        return;
+    }
+}
--- practice-elte-2023-spring/biro/air pollution deterioration/Program.cs
+++ practice-elte-2023-spring/biro/air pollution deterioration/Program.cs
@@ -3,31 +3,31 @@ using System.Linq;
 
 class Programm
 {
-
     static void Main()
     {
-        int days, result = 0;
-        int prev_morning = 0, prev_evening = 0, curr_morning = 0, curr_evening = 0;
-        string[] temp;
+        int days,
+            curr_morning, curr_evening,
+            prev_morning, prev_evening;
+        int result = 0;
 
         days = Convert.ToInt32(Console.ReadLine());
 
-
+        int[,] aq = new int[days, 2];
+        string[] temp;
 
         for (int d = 0; d < days; d++)
         {
-
             temp = Console.ReadLine().Split(" ");
-            curr_morning = Convert.ToInt32(temp[0]);
-            curr_evening = Convert.ToInt32(temp[1]);
+            aq[d, 0] = Convert.ToInt32(temp[0]);
+            aq[d, 1] = Convert.ToInt32(temp[1]);
+        }
 
-            if (prev_morning == 0 && prev_evening == 0)
-            {
-                prev_morning = curr_morning;
-                prev_evening = curr_evening;
-                continue;
-            }
-
+        for (int i = 1; i < days; i++)
+        {
+            prev_morning = aq[i - 1, 0];
+            prev_evening = aq[i - 1, 1];
+            curr_morning = aq[i, 0];
+            curr_evening = aq[i, 1];
             if (
                 curr_morning > prev_morning &&
                 curr_evening > prev_evening
@@ -35,13 +35,8 @@ class Programm
             {
                 result = result + 1;
             }
-
-            prev_morning = curr_morning;
-            prev_evening = curr_evening;
-
         }
 
         Console.Write($"{result}\n");
-
     }
 }
