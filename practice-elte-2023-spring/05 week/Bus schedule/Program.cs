5042c58ce3d9169c23c82a2f3b8fa8464b832220
Euryn <crossdeltavi@gmail.com>
2023-04-03 11:08:26 +0200

    solved two more exercises from week 5

---
+++ practice-elte-2023-spring/05 week/Bus schedule/Program.cs
@@ -0,0 +1,45 @@
+﻿// 7
+// Budapest -1 480
+// Dunaujvaros 549 550
+// Paks 580 582
+// Szekszard 9 600
+// Szekszard 604 605
+// Mohacs 650 9999
+// Mohacs 655 999
+
+class Program
+{
+    static void Main()
+    {
+        bool exists = false;
+        int N, departure;
+        string buffer, town;
+        string[] splitted_buffer = new string[4];
+
+        buffer = Console.ReadLine();
+        N = Convert.ToInt32(buffer);
+
+        for (int i = 0; i < N; i++)
+        {
+            buffer = Console.ReadLine();
+            splitted_buffer = buffer.Split(" ");
+
+            town = splitted_buffer[0];
+            departure = Convert.ToInt32(splitted_buffer[1]);
+
+            if (town == "Szekszard" && departure == -1)
+            {
+                exists = true;
+
+            }
+        }
+        if (exists)
+        {
+            Console.Write($"YES\n");
+        }
+        else
+        {
+            Console.Write($"NO\n");
+        }
+    }
+}
---
+++ practice-elte-2023-spring/05 week/Total dispensed money/Program.cs
@@ -0,0 +1,44 @@
+﻿// 7
+// 11 10 1 12345
+// 11 50 -2 10000
+// 12 10 -1 12345
+// 23 10 1 24680
+// 23 11 -2 5000
+// 23 15 1 13579
+// 23 20 1 98765
+
+class Program
+{
+    static void Main()
+    {
+        int N, mode, amount, total_dispensed = 0;
+        string buffer;
+        string[] splitted_buffer = new string[4];
+
+        buffer = Console.ReadLine();
+        N = Convert.ToInt32(buffer);
+
+
+        for (int i = 0; i < N; i++)
+        {
+            buffer = Console.ReadLine();
+            splitted_buffer = buffer.Split(" ");
+
+            mode = Convert.ToInt32(splitted_buffer[2]);
+            amount = Convert.ToInt32(splitted_buffer[3]);
+
+
+            if (mode == -2)
+            {
+                total_dispensed += amount;
+            }
+            else if (mode == 2)
+            {
+                total_dispensed -= amount;
+            }
+
+        }
+
+        Console.Write($"{total_dispensed}\n");
+    }
+}
