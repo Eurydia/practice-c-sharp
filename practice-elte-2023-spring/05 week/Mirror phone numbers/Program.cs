782b8098a60e8eb84ef1a911784f39548f0c909a
Euryn <crossdeltavi@gmail.com>
2023-04-03 22:04:37 +0200

    added readme for "stormy days"

---
+++ practice-elte-2023-spring/05 week/Mirror phone numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿// 8
+// 11811886
+// 13977931
+// 64719635
+// 88922988
+// 68874786
+// 33842127
+// 37895247
+// 56362263
+
+class Program
+{
+
+    static bool isMirror(string s)
+    {
+        for (int i = 0; i < (s.Length / 2); i++)
+        {
+            if (s[i] != s[s.Length])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+
+        int N;
+        string buffer, curr_number;
+
+        buffer = Console.ReadLine();
+        N = Convert.ToInt32(buffer);
+
+        string[] numbers = new string[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            buffer = Console.ReadLine();
+            numbers[i] = buffer;
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            curr_number = numbers[i];
+
+            if (curr_number[0] == '1' && isMirror(curr_number.Substring(1)))
+            {
+                Console.Write($"{curr_number}\n");
+            }
+            else if (isMirror(curr_number.Substring(2)))
+            {
+                Console.Write($"{curr_number}\n");
+            }
+        }
+    }
+}
---
+++ practice-elte-2023-spring/05 week/Stormy days/readme.md
@@ -0,0 +1,45 @@
+
+# Stormy days
+
+The organizers of a sailing competition measured the speed of wind for N days at Balaton-
+füred. 
+A sailing competition should not be held in smooth winds, nor in stormy winds. 
+In a storm, the speed of wind is more than 100 km/h. 
+There is only one round each day of the competition, and there are K rounds.
+
+Write a program that counts how many days the wind was stormy.
+
+## Input
+The frst line of the standard input contains the count of days (1≤N≤100) and the
+competition days (1≤K≤10). The next N lines contain the speed of wind of each day
+(1≤S≤200).
+
+## Output
+
+The frst line of the standard output should contain the count of days with a storm.
+
+## Example
+
+**Input**
+10 3
+50
+40
+0
+5
+0
+80
+70
+90
+100
+120
+
+**Output**
+1
+
+**Limits**
+
+Time limit: 0.1 second
+
+Memory limit: 32 MB
+
+Evaluation: In 40% of tests, the count of data is ≤20
