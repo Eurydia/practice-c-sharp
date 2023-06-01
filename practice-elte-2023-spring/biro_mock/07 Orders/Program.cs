b24b71ac3adcd877cc5381a175d2b5748be39ded
Euryn <crossdeltavi@gmail.com>
2023-06-01 18:08:50 +0200

    solved more biro_mock

---
+++ practice-elte-2023-spring/biro_mock/07 Orders/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+
+struct Offer
+{
+    public int companyId;
+    public int productId;
+    public int productPrice;
+}
+
+
+class Program
+{
+
+    static void SolveTaskA(int offerCount, Offer[] offers)
+    {
+        int i;
+        int currPrice, currId;
+
+        int mostExpensiveId = offers[0].productId;
+        int mostExpensivePrice = offers[0].productPrice;
+
+        for (i = 1; i < offerCount; i++)
+        {
+            currId = offers[i].productId;
+            currPrice = offers[i].productPrice;
+
+            if (currPrice < mostExpensivePrice)
+            {
+                continue;
+            }
+            else if (currPrice == mostExpensivePrice)
+            {
+                if (currId < mostExpensiveId)
+                {
+                    mostExpensiveId = currId;
+                }
+            }
+            else
+            {
+                mostExpensiveId = currId;
+                mostExpensivePrice = currPrice;
+            }
+        }
+        Console.WriteLine("#");
+        Console.WriteLine($"{mostExpensiveId}");
+    }
+
+    static void SolveTaskB(int companyCount, int offerCount, Offer[] offers)
+    {
+        int i, currId;
+
+        int[] productProducers = new int[companyCount];
+        for (i = 0; i < offerCount; i++)
+        {
+            currId = offers[i].companyId - 1;
+            productProducers[currId]++;
+        }
+        int mostProductId = 1;
+        int mostProduct = productProducers[0];
+        for (i = 1; i < companyCount; i++)
+        {
+            if (productProducers[i] > mostProduct)
+            {
+                mostProductId = i + 1;
+                mostProduct = productProducers[i];
+            }
+        }
+        Console.WriteLine("#");
+        Console.WriteLine($"{mostProductId}");
+    }
+
+    static void SolveTaskC(int productCount, int offerCount, Offer[] offers)
+    {
+        int i, currId;
+
+        int[] producedBy = new int[productCount];
+        for (i = 0; i < offerCount; i++)
+        {
+            currId = offers[i].productId - 1;
+            producedBy[currId]++;
+        }
+
+        int uniqueProductCount = 0;
+        int[] uniqueProductIds = new int[productCount];
+        for (i = 0; i < productCount; i++)
+        {
+            if (producedBy[i] == 1)
+            {
+                uniqueProductIds[uniqueProductCount] = i + 1;
+                uniqueProductCount++;
+            }
+        }
+
+        Console.WriteLine("#");
+        Console.WriteLine($"{uniqueProductCount}");
+        Array.Sort(uniqueProductIds);
+        for (i = (productCount - uniqueProductCount); i < productCount; i++)
+        {
+            Console.Write($"{uniqueProductIds[i]} ");
+        }
+        Console.Write("\n");
+    }
+
+    static void SolveTaskD(int productCount, int companyCount, int offerCount, Offer[] offers)
+    {
+        int i, j;
+        int[][] productTable = new int[companyCount][];
+        for (i = 0; i < companyCount; i++)
+        {
+            productTable[i] = new int[productCount];
+        }
+
+        int row, col;
+        for (i = 0; i < offerCount; i++)
+        {
+            row = offers[i].companyId - 1;
+            col = offers[i].productId - 1;
+            productTable[row][col] = 1;
+        }
+
+        Console.Write("#\n");
+        for (i = 0; i < companyCount; i++)
+        {
+            for (j = i + 1; j < companyCount; j++)
+            {
+                if (productTable[i].SequenceEqual(productTable[j]))
+                {
+                    Console.Write($"{i + 1} {j + 1}\n");
+                    return;
+                }
+            }
+        }
+        Console.Write("-1\n");
+    }
+
+    static void Main(string[] args)
+    {
+        int i;
+        string buffer;
+        string[] bufferSplitted;
+
+        int N, M, O;
+        buffer = Console.ReadLine()!;
+        bufferSplitted = buffer.Split(" ");
+        N = Convert.ToInt32(bufferSplitted[0]);
+        M = Convert.ToInt32(bufferSplitted[1]);
+        O = Convert.ToInt32(bufferSplitted[2]);
+
+
+        Offer[] offers = new Offer[O];
+        for (i = 0; i < O; i++)
+        {
+            buffer = Console.ReadLine()!;
+            bufferSplitted = buffer.Split(" ");
+
+
+            offers[i].companyId = Convert.ToInt32(bufferSplitted[0]);
+            offers[i].productId = Convert.ToInt32(bufferSplitted[1]);
+            offers[i].productPrice = Convert.ToInt32(bufferSplitted[2]);
+        }
+
+        Program.SolveTaskA(O, offers);
+        Program.SolveTaskB(N, O, offers);
+        Program.SolveTaskC(N, O, offers);
+        Program.SolveTaskD(N, M, O, offers);
+    }
+}
---
+++ practice-elte-2023-spring/biro_mock/08 Rain/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+
+    static void SolveTaskA(int weekCount, int[,] rainAmount)
+    {
+        int i, j, rainSum;
+        string result = "";
+
+        for (i = 0; i < weekCount; i++)
+        {
+            rainSum = 0;
+            for (j = 0; j < 7; j++)
+            {
+                rainSum += rainAmount[i, j];
+            }
+            result = $"{result}{rainSum} ";
+        }
+        Console.Write($"{result}\n");
+    }
+
+    static void SolveTaskB(int weekCount, int[,] rainAmount)
+    {
+        int i, j;
+
+        int[] rainSums = new int[weekCount];
+        for (i = 0; i < weekCount; i++)
+        {
+            for (j = 0; j < 7; j++)
+            {
+                rainSums[i] = rainAmount[i, j];
+            }
+        }
+        int mostRain = rainSums[0];
+        int mostRainIndex = 1;
+        for (i = 1; i < weekCount; i++)
+        {
+            if (rainSums[i] > mostRain)
+            {
+                mostRainIndex = i + 1;
+                mostRain = rainSums[i];
+            }
+        }
+        Console.Write($"{mostRainIndex}\n");
+    }
+
+    static void SolveTaskC(int weekCount, int[,] rainAmount)
+    {
+        int i, j;
+
+        int increaseRainCount = 0;
+        int[] increaseRainWeeks = new int[weekCount];
+
+        for (i = 0; i < weekCount; i++)
+        {
+            for (j = 0; j < 6; j++)
+            {
+                if (rainAmount[i, j] > rainAmount[i, j + 1])
+                {
+                    break;
+                }
+            }
+            if (j == 6)
+            {
+                increaseRainWeeks[increaseRainCount] = i + 1;
+                increaseRainCount++;
+            }
+        }
+        Console.Write($"{increaseRainCount}");
+        for (i = 0; i < increaseRainCount; i++)
+        {
+            Console.Write($" {increaseRainWeeks[i]}");
+        }
+        Console.Write("\n");
+    }
+
+    static void SolveTaskD(int weekCount, int[,] rainAmount)
+    {
+        int i, j;
+
+        int[] rainSums = new int[weekCount];
+        for (i = 0; i < weekCount; i++)
+        {
+            for (j = 0; j < 7; j++)
+            {
+                rainSums[i] = rainAmount[i, j];
+            }
+        }
+        int leastRain = rainSums[0];
+        int leastRainIndex = 1;
+        for (i = 1; i < weekCount / 2; i++)
+        {
+            if (rainSums[i] < leastRain)
+            {
+                leastRainIndex = i + 1;
+                leastRain = rainSums[i];
+            }
+        }
+        Console.Write($"{leastRainIndex}\n");
+    }
+
+    static void SolveTaskE(int weekCount, int[,] rainAmount)
+    {
+        int i, j;
+
+        int[] rainSums = new int[weekCount];
+        for (i = 0; i < weekCount; i++)
+        {
+            for (j = 0; j < 7; j++)
+            {
+                rainSums[i] = rainAmount[i, j];
+            }
+        }
+
+        int periodLength = 0;
+
+        int periodStartLongest = 0;
+        int periodLengthLongest = 0;
+
+        for (i = 0; i < weekCount; i++)
+        {
+            if (rainSums[i] > 10)
+            {
+                continue;
+            }
+            periodLength = 0;
+            for (j = i; j < weekCount; j++)
+            {
+                periodLength++;
+                if (periodLength > periodLengthLongest)
+                {
+                    periodLengthLongest = periodLength;
+                    periodStartLongest = i + 1;
+                }
+            }
+        }
+
+        if (periodStartLongest == 0)
+        {
+            Console.Write("0\n");
+        }
+        else
+        {
+            Console.WriteLine($"{periodStartLongest} {periodStartLongest + periodLengthLongest - 1}\n");
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        int i, j;
+        string buffer;
+        string[] bufferSplitted;
+
+        int N;
+
+        buffer = Console.ReadLine();
+        N = Convert.ToInt32(buffer);
+
+        int[,] rainAmounts = new int[N, 7];
+        for (i = 0; i < N; i++)
+        {
+            buffer = Console.ReadLine();
+            bufferSplitted = buffer.Split(" ");
+            for (j = 0; j < 7; j++)
+            {
+                rainAmounts[i, j] = Convert.ToInt32(bufferSplitted[j]);
+            }
+        }
+
+        SolveTaskA(N, rainAmounts);
+        SolveTaskB(N, rainAmounts);
+        SolveTaskC(N, rainAmounts);
+        SolveTaskD(N, rainAmounts);
+        SolveTaskE(N, rainAmounts);
+
+    }
+}
